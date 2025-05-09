9e4d7cb967ad1ffb19223557d19c680933e8fed5
Miroslav Pecenka <pecenka.miroslav@gmail.com>
2025-05-09 02:59:01 +0200

    Added: some functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,32 @@
+use std::io;
+
+fn menu() {
+    println!("1. Přihlásit");
+    println!("2. Registrovat");
+}
+
+fn prihlasit() {
+
+}
+
+fn registrovat() {
+    
+}
+
 fn main() {
-    println!("Welcome in the System!");
-    println!("Would you like to log-in?:");
+    // Zobrazí menu
+    menu();
+
+    let mut volba = String::new();
+
+    io::stdin().read_line(&mut volba).expect("Zápis selhal");
+
+    //println!("Zvoleno: {}", volba.trim());
+
+    match volba.trim()  {
+        "1" => println!("Zvoleno přihlášení"),
+        "2" => println!("Zvolena registrace"),
+        _ => println!("CHYBA: Zvolte výběr z menu!"),
+    }
+
 }
