cce1c22eff18c676e79c9c5f1828a6b67e6b24a4
Miroslav Pecenka <pecenka.miroslav@gmail.com>
2025-05-10 02:58:50 +0200

    Added: some stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::process::Command;
 use std::io;
 
+/// Clear terminal like 'cls'
 fn clear() {
     if cfg!(target_os = "windows") {
         Command::new("cmd").args(["/C", "cls"]).status().unwrap();
@@ -9,37 +10,66 @@ fn clear() {
     }
 }
 
-fn show_menu() {
+/// Show menu and return input
+fn show_menu() -> String {
+    let mut choice = String::new();
+    println!("1. Registrovat se");
+    println!("2. Zobrazit seznam");
+    println!("3. Ukončit");
+    io::stdin().read_line(&mut choice).unwrap();
     clear();
-    println!("1. Log-in");
-    println!("2. Sign-up");
+    return choice;
+}
+
+fn registrace() -> String {
+    let mut prezdivka = String::new();
+    println!("Zadej přezdívku:");
+    io::stdin().read_line(&mut prezdivka).unwrap();
+    clear();
+    return prezdivka;
 }
 
 fn main() {
-    // Show menu
-    show_menu();
+    clear(); // Clear terminal for first use
+    let mut user_list: Vec<String> = vec![];
 
     loop {
-        // Variable with input
-        let mut choice = String::new();
-        io::stdin().read_line(&mut choice).expect("Write failed");
+        clear();
+        let volba = show_menu();  // Zavolá menu a input
 
-        // Matching user's output
-        match choice.trim()  {
-            "1" => {
-                clear();
-                println!("Sucessful log-in!");
+        match volba.trim() {
+            "1" => loop {
+                let udaje = registrace();
+                user_list.push(udaje);
+                println!("ÚSPĚŠNĚ REGISTROVÁNO!");
+               
                 break;
             }
-            "2" => {
+            "2" => { loop {
+                    for x in &user_list {
+                        print!("{}", x);
+                    }
+                    println!("Přejete se vrátit zpět do menu? (ano/ne)");
+                    let mut vratit_zpet = String::new();
+                    io::stdin().read_line(&mut vratit_zpet).unwrap();
+                    match vratit_zpet.trim() {
+                        "ano" => break,
+                        _ => continue,
+                    }
+                }
+            }
+            "3" => {
                 clear();
-                println!("Welcome in Registration!");
-                break
+                println!("PROGRAM UKONČEN!");
+                break;
             }
-            _ => {
-                show_menu();
-                println!("ERROR: CHOOSE 1 or 2!");
-            }
-        }   
+            _ => println!("ERROR"),
+        }
+
+
     }
+    
+    
+
+
 }
